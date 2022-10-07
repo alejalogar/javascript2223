b0c4de92d0071c51b1585708d7ad06e4a5fb0466
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2022-10-07 10:08:18 +0200

    ej06 funcional

--- ej06-lista-carrito-compra/scripts/ej06.js
+++ ej06-lista-carrito-compra/scripts/ej06.js
@@ -1,5 +1,7 @@
 const txtAdd = document.querySelector("#txtAdd")
 const mylist = document.querySelector("#mylist")
+const mycart = document.querySelector("#mycart")
+
 const btnAdd = document.querySelector("#btnAdd")
 //Recuperar los 5 botones de acciones de la lista de la compra
 const btnSelAll = document.querySelector("#btnSelAll")
@@ -7,33 +9,12 @@ const btnSelNot = document.querySelector("#btnSelNot")
 const btnInvSel = document.querySelector("#btnInvSel")
 const btnMovSel = document.querySelector("#btnMovSel")
 const btnDelSel = document.querySelector("#btnDelSel")
+const btnEmpCar = document.querySelector("#btnEmpCar")
 
-btnSelAll.addEventListener("click",function(){
-    
-})
-btnSelNot.addEventListener("click",function(){
-    
-})
-btnInvSel.addEventListener("click",function(){
-    
-})
-btnMovSel.addEventListener("click",function(){
-    
-})
-btnDelSel.addEventListener("click",function(){
-    let selectedLIS = document.querySelectorAll("#mylist>li.seleccionado")
-    selectedLIS.forEach(li => li.remove())
-})
-
-txtAdd.focus()
-
-txtAdd.addEventListener("keyup",function(evento){
-    if (evento.key == "Enter")
-        addItemToList()
-})
-
+/* ***********
+    BOTONES
+************** */
 btnAdd.addEventListener("click",addItemToList)
-
 function addItemToList(){
     let texto = txtAdd.value.trim()
     if (texto.length) {
@@ -42,7 +23,7 @@ function addItemToList(){
         nuevoLI.textContent = texto
         mylist.append(nuevoLI)
         //darle al nuevo LI comportamiento ante los clics
-        nuevoLI.addEventListener("click",function(){
+        nuevoLI.addEventListener("click", function liclicable(){
             /*
             if (this.classList.contains("seleccionado")) {
                 //ya la tiene, se la quito
@@ -57,4 +38,48 @@ function addItemToList(){
         txtAdd.focus()
     }
 }
+btnSelAll.addEventListener("click",function(){
+    let todosLosLI = document.querySelectorAll("#mylist>li")
+    todosLosLI.forEach(li => li.classList.add("seleccionado"))
+})
+btnSelNot.addEventListener("click",function(){
+    let todosLosLI = document.querySelectorAll("#mylist>li")
+    todosLosLI.forEach(li => li.classList.remove("seleccionado"))
+})
+btnInvSel.addEventListener("click",function(){
+    let todosLosLI = document.querySelectorAll("#mylist>li")
+    todosLosLI.forEach(li => li.classList.toggle("seleccionado"))
+})
+btnMovSel.addEventListener("click",function(){
+    let selectedLIS = document.querySelectorAll("#mylist>li.seleccionado")
+    selectedLIS.forEach(li => {
+        //mycart.append(li) //lo corta y lo pega en la lista de abajo
+        // pero sigue siendo clicable y no es lo que deseamos
+
+        let nuevoLI = document.createElement("LI")
+        nuevoLI.textContent = li.textContent
+        mycart.append(nuevoLI)
+        li.remove()
+    })
+})
+btnDelSel.addEventListener("click",function(){
+    let selectedLIS = document.querySelectorAll("#mylist>li.seleccionado")
+    selectedLIS.forEach(li => li.remove())
+})
+btnEmpCar.addEventListener("click",function(){
+    mycart.innerHTML = ""
+})
+
+
+
+/* *******************************
+    CÓDIGO PRINCIPAL DE LA PÁGINA
+************************************  */
+txtAdd.focus()
+txtAdd.addEventListener("keyup",function(evento){
+    if (evento.key == "Enter")
+        addItemToList()
+})
+
+
 
