9c30ec5b8cf802961ec28c6917b9994ea3e2f0c3
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2022-10-07 09:27:13 +0200

    implementando botones de lista de la compra

--- ej06-lista-carrito-compra/scripts/ej06.js
+++ ej06-lista-carrito-compra/scripts/ej06.js
@@ -1,6 +1,29 @@
 const txtAdd = document.querySelector("#txtAdd")
 const mylist = document.querySelector("#mylist")
 const btnAdd = document.querySelector("#btnAdd")
+//Recuperar los 5 botones de acciones de la lista de la compra
+const btnSelAll = document.querySelector("#btnSelAll")
+const btnSelNot = document.querySelector("#btnSelNot")
+const btnInvSel = document.querySelector("#btnInvSel")
+const btnMovSel = document.querySelector("#btnMovSel")
+const btnDelSel = document.querySelector("#btnDelSel")
+
+btnSelAll.addEventListener("click",function(){
+    
+})
+btnSelNot.addEventListener("click",function(){
+    
+})
+btnInvSel.addEventListener("click",function(){
+    
+})
+btnMovSel.addEventListener("click",function(){
+    
+})
+btnDelSel.addEventListener("click",function(){
+    let selectedLIS = document.querySelectorAll("#mylist>li.seleccionado")
+    selectedLIS.forEach(li => li.remove())
+})
 
 txtAdd.focus()
 
@@ -18,8 +41,20 @@ function addItemToList(){
         const nuevoLI = document.createElement("LI")
         nuevoLI.textContent = texto
         mylist.append(nuevoLI)
+        //darle al nuevo LI comportamiento ante los clics
+        nuevoLI.addEventListener("click",function(){
+            /*
+            if (this.classList.contains("seleccionado")) {
+                //ya la tiene, se la quito
+                this.classList.remove("seleccionado")
+            } else {
+                this.classList.add("seleccionado")
+            }*/
+            this.classList.toggle("seleccionado")
+        })
         //vaciar la caja de texto y darle el foco de nuevo
         txtAdd.value = ""
         txtAdd.focus()
     }
 }
+
