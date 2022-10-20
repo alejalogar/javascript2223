484272b05d798084bbd13a5b12101bdaf21fd34e
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2022-10-20 10:06:57 +0200

    ej09 terminado

--- ej09-billar/scripts/ej09.js
+++ ej09-billar/scripts/ej09.js
@@ -2,8 +2,30 @@ const ANCHURA_TABLERO = 600
 const ALTURA_TABLERO = 300
 const DIAMETRO_BOLA = 30
 const tablero = document.querySelector("#tablero")
+const remBallBtn = document.querySelector("#remBallBtn")
+const addBallBtn = document.querySelector("#addBallBtn")
+const rem10BallsBtn = document.querySelector("#rem10BallsBtn")
+const add10BallsBtn = document.querySelector("#add10BallsBtn")
+const ballCounter = document.querySelector("#ballCounter")
 
 let bolas = []
+//inicializar contador a 0
+ballCounter.value = 0
+//asignar comportamiento a botones remBall y addBall
+addBallBtn.addEventListener("click",()=>{
+    addBall()
+})
+add10BallsBtn.addEventListener("click",()=>{
+    for (let i=0; i<10; i++)
+        addBall()
+})
+remBallBtn.addEventListener("click",()=>{
+    remBall()
+})
+rem10BallsBtn.addEventListener("click",()=>{
+    for (let i=0; i<10; i++)
+        remBall()
+})
 
 function addBall() {
     //incorporar un nuevo DIV bola al árbol DOM
@@ -16,6 +38,7 @@ function addBall() {
     let velY = Math.random()*2 - 1
     let newBall = new Bola(newDOMball,bolaX,bolaY,velX,velY)
     bolas.push(newBall)
+    ballCounter.value++
     //ajustar propiedades CSS del DIV recién creado
     newDOMball.style.left = bolaX + "px"
     newDOMball.style.top = bolaY + "px"
@@ -27,15 +50,13 @@ function addBall() {
     newDOMball.classList.add("bola")
 }
 
-addBall()
-addBall()
-addBall()
-
-
 function remBall() {
-
+    if (bolas.length) {
+        bolas[0].bola.remove()
+        bolas.shift()
+        ballCounter.value--
+    }
 }
-
 
 function moveBall() {
     bolas.forEach(b => {
