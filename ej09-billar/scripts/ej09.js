f7f242592ba62cb17497fbdccbb39fc8664845af
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2022-10-20 08:26:16 +0200

    ej09 casi terminado

--- ej09-billar/scripts/ej09.js
+++ ej09-billar/scripts/ej09.js
@@ -1,26 +1,55 @@
 const ANCHURA_TABLERO = 600
 const ALTURA_TABLERO = 300
 const DIAMETRO_BOLA = 30
+const tablero = document.querySelector("#tablero")
 
-const bola = document.querySelector("#bola")
-let bolaX = 0
-let velX = 1 //mover a la derecha
-bola.style.left = bolaX + "px"
-let bolaY = 0
-let velY = 1 //mover hacia abajo
-bola.style.top = bolaY + "px"
+let bolas = []
 
-let primeraLeyDeLaTermidinamica = setInterval(moverBola,10)
-
-function moverBola() {
-    bolaY += velY
-    bola.style.top = bolaY + "px"
-    bolaX += velX
-    bola.style.left = bolaX + "px"
-    //si alcanza el borde superior o inferior, cambiar de dirección
-    if (bolaY > ALTURA_TABLERO - DIAMETRO_BOLA || bolaY < 0)
-        velY = -velY
-    //si alcanza el borde izquierdo o derecho, cambiar de dirección
-    if (bolaX > ANCHURA_TABLERO - DIAMETRO_BOLA || bolaX < 0)
-        velX = -velX
+function addBall() {
+    //incorporar un nuevo DIV bola al árbol DOM
+    let newDOMball = document.createElement("DIV")
+    tablero.append(newDOMball)
+    //incorporar una nueva instancia de Bola al array "bolas"
+    let bolaX = Math.random()* (ANCHURA_TABLERO-DIAMETRO_BOLA)
+    let bolaY = Math.random()* (ALTURA_TABLERO-DIAMETRO_BOLA)
+    let velX = Math.random()*2 - 1
+    let velY = Math.random()*2 - 1
+    let newBall = new Bola(newDOMball,bolaX,bolaY,velX,velY)
+    bolas.push(newBall)
+    //ajustar propiedades CSS del DIV recién creado
+    newDOMball.style.left = bolaX + "px"
+    newDOMball.style.top = bolaY + "px"
+    let r = Math.random()*255
+    let g = Math.random()*255
+    let b = Math.random()*255
+    //newDOMball.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")"
+    newDOMball.style.backgroundColor = `rgb(${r},${g},${b})`
+    newDOMball.classList.add("bola")
 }
+
+addBall()
+addBall()
+addBall()
+
+
+function remBall() {
+
+}
+
+
+function moveBall() {
+    bolas.forEach(b => {
+        b.posY += b.velY
+        b.posX += b.velX
+        b.bola.style.top = b.posY + "px"
+        b.bola.style.left = b.posX + "px"
+        //si alcanza el borde superior o inferior, cambiar de dirección
+        if (b.posY > ALTURA_TABLERO - DIAMETRO_BOLA || b.posY < 0)
+            b.velY = -b.velY
+        //si alcanza el borde izquierdo o derecho, cambiar de dirección
+        if (b.posX > ANCHURA_TABLERO - DIAMETRO_BOLA || b.posX < 0)
+            b.velX = -b.velX
+    })
+}
+
+let primeraLeyDeLaTermidinamica = setInterval(moveBall,5)
