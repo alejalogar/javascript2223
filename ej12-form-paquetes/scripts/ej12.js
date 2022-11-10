dde1c0b4a0a8dea009f634aa5f8fd0dba3aaec3f
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2022-11-10 09:30:42 +0100

    ej12 casi terminado

--- ej12-form-paquetes/scripts/ej12.js
+++ ej12-form-paquetes/scripts/ej12.js
@@ -8,28 +8,32 @@ formEnvios.action = "todoOK.html"
 
 formEnvios.addEventListener("submit",function(ev){
     ev.preventDefault() //impedir que el formulario se envíe
-    //vaciar los LABEL
-    let labelsDelForm = document.querySelectorAll("#formEnvios label")
-    labelsDelForm.forEach(label => label.textContent = "")
-
-    //comprobaciones
-    let todoOK = true
-
-    if (!numrefOK()) {
-        todoOK = false
-        //advertir al usuario
-        refPedidoError.textContent = "Debe seguir el patrón A12345"
-    }
-
-    if (!pesoOK()) {
-        todoOK = false
-        //advertir al usuario
-        pesoError.textContent = "Debe seguir el patrón 0.0 o 0,0 o 00.0 o 00,0"
-    }
 
     //si todo OK entonces lanzo el submit()
-    if (todoOK) this.submit()
+    if (todoOK()) this.submit()
 })
+
+function todoOK() {
+        //vaciar los LABEL
+        let labelsDelForm = document.querySelectorAll("#formEnvios label")
+        labelsDelForm.forEach(label => label.textContent = "")
+    
+        //comprobaciones
+        let todoOK = true
+    
+        if (!numrefOK()) {
+            todoOK = false
+            //advertir al usuario
+            refPedidoError.textContent = "Debe seguir el patrón A12345"
+        }
+    
+        if (!pesoOK()) {
+            todoOK = false
+            //advertir al usuario
+            pesoError.textContent = "Debe seguir el patrón 0.0 o 0,0 o 00.0 o 00,0"
+        }
+        return todoOK
+}
 
 function numrefOK() {
     let numOK = true
@@ -49,3 +53,36 @@ function pesoOK() {
     let expreg = /^[0-9]{1,2}[\.,][0-9]$/
     return expreg.test(peso.value)
 }
+
+//Recuperar la tabla y el botón "Añadir a tabla"
+const tablaEnvios = document.querySelector("#tablaEnvios")
+const anyadir = document.querySelector("#anyadir")
+
+anyadir.addEventListener("click", function(){
+    if (todoOK()) {
+        //crear fila en el tbody
+        let nuevaFila = tablaEnvios.querySelector("tbody").insertRow()
+        //Primera celda: num de referencia del pedido
+        let celdaNumRef = nuevaFila.insertCell()
+        celdaNumRef.textContent = refPedido.value
+        //Segunda celda: peso del pedido
+        let celdaPeso = nuevaFila.insertCell()
+        celdaPeso.textContent = peso.value
+        //Tercera celda: botón para borrar fila
+        let celdaOpciones = nuevaFila.insertCell()
+
+        celdaOpciones.innerHTML = "<button>Borrar fila</button>"
+        let botonBorrar = celdaOpciones.querySelector("button")
+
+        /* las 2 líneas anteriores equivalen a estas 3:
+        let botonBorrar = document.createElement("button")
+        botonBorrar.textContent = "Borrar fila"
+        celdaOpciones.append(botonBorrar) */
+
+        botonBorrar.addEventListener("click",function(){
+            nuevaFila.remove()
+            /* equivalente a:
+            this.parentNode.parentNode.remove() */
+        })
+    }
+})
