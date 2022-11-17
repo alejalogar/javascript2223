71478ef1ef20a680e17c3b003149f20ad87b459b
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2022-11-17 10:06:57 +0100

    ej14 funcionando

--- ej14-fetch-gpushop/scripts/ej14.js
+++ ej14-fetch-gpushop/scripts/ej14.js
@@ -1,6 +1,8 @@
 const buscador = document.querySelector("#buscador")
 const cuerpoResultados = document.querySelector("#tableResultados>tbody")
 const cuerpoSeleccionados = document.querySelector("#tableSeleccionados>tbody")
+const inputBorrarTodos = document.querySelector("#inputBorrarTodos")
+const pieSeleccionados = document.querySelector("#tableSeleccionados>tfoot")
 
 //dar foco al input de búsqueda
 buscador.focus()
@@ -18,14 +20,54 @@ buscador.addEventListener("keyup",function(ev){
 })
 
 //delegamos el evento "click sobre los botones añadir"
-// al TBODY, por ejemplo, que es bisabuelo de los botones añadir
+// al TBODY de la tabla RESULTADOS, que es bisabuelo de los botones añadir
 cuerpoResultados.addEventListener("click",function(ev){
     if (ev.target.nodeName.toLowerCase() == "button") {
         let producto = ev.target.parentNode.parentNode.children[0].textContent
         let precio = ev.target.parentNode.parentNode.children[1].textContent
         anyadirProductoACarrito(producto,precio)
+        actualizarTotales()
     }
 })
+
+//delegamos el evento "click sobre los botones borrar"
+// al TBODY de la tabla SELECCIONADOS, que es bisabuelo de los botones borrar
+cuerpoSeleccionados.addEventListener("click",function(ev){
+    if (ev.target.nodeName.toLowerCase() == "button") {
+        ev.target.parentNode.parentNode.remove()
+        actualizarTotales()
+    }
+})
+
+inputBorrarTodos.addEventListener("click",function(){
+    cuerpoSeleccionados.innerHTML = ""
+    actualizarTotales()
+})
+
+function actualizarTotales() {
+    let filas = Array.from(cuerpoSeleccionados.children)
+    let totalNumProductos = filas.length
+
+    let totalPrecio = 0
+    filas.forEach(fila=>{
+        //extraer el precio
+        let precio = fila.children[1].textContent
+        precio = precio.substring(0,precio.length-2)
+        //sumar al total
+        totalPrecio += parseFloat(precio)
+    })
+
+    //llevar esos totales al TFOOT
+    let celdaNumProductos = pieSeleccionados.children[0].children[0]
+    let celdaTotalPrecio = pieSeleccionados.children[0].children[1]
+    
+    if (totalNumProductos == 1)
+        celdaNumProductos.textContent = "1 artículo"
+    else 
+        celdaNumProductos.textContent = totalNumProductos + " artículos"
+    
+    celdaTotalPrecio.textContent = totalPrecio + " €"
+}
 
 function anyadirProductoACarrito(producto,precio) {
     let unaFila = cuerpoSeleccionados.insertRow()
