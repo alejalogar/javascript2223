3fbe5c1f5f0fe0c875de7d99075cac3a0dfdea71
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2022-11-22 14:44:25 +0100

    ej16 planteado

--- ej15-fetch-municipios/scripts/ej15.js
+++ ej15-fetch-municipios/scripts/ej15.js
@@ -1,4 +1,5 @@
 const selectProvincias = document.querySelector("#provincias")
+const selectMunicipios = document.querySelector("#municipios")
 
 fetch("server/cargaProvinciasXML.php")
 .then(respuestaCruda => {
@@ -21,3 +22,32 @@ fetch("server/cargaProvinciasXML.php")
         })
     }
 )
+
+selectProvincias.addEventListener("change",function(ev){
+    //primero vaciamos el SELECT municipios
+    selectMunicipios.innerHTML = '<option value="0">(Elige municipio)</option>'
+
+    let codProvElegida = this.value
+
+    const params = new URLSearchParams("provincia="+codProvElegida) //provincia=04
+    const options = {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        body: params // body data type must match "Content-Type" header
+    }
+    fetch("server/cargaMunicipiosXML.php",options)
+    .then(resp => resp.text())
+    .then(textoCrudoXML => {
+        let parser = new DOMParser()
+        let xml = parser.parseFromString(textoCrudoXML,"text/xml")
+        //recuperar los municipios del árbol "xml"
+        let municipios = xml.querySelectorAll("municipio")
+        municipios.forEach(municipio => {
+             let codigo = municipio.children[0].textContent
+             let nombre = municipio.children[1].textContent
+             let newOption = document.createElement("OPTION")
+             newOption.value = codigo
+             newOption.textContent = nombre
+             selectMunicipios.append(newOption)
+        })
+    })
+})
