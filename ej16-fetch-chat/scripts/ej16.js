076b13972cb381ea3793221fbb8bdfd38bb44162
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2022-11-25 09:33:57 +0100

    ej17 planteao

--- ej16-fetch-chat/scripts/ej16.js
+++ ej16-fetch-chat/scripts/ej16.js
@@ -1,6 +1,8 @@
 const nick = document.querySelector("#nick")
 const teclado = document.querySelector("#teclado")
 const chat = document.querySelector("#chat")
+let ultimoRecibido = 0
+
 
 //capturar el INTRO en el INPUT del mensaje 
 teclado.addEventListener("keyup",function(ev){
@@ -20,14 +22,15 @@ teclado.addEventListener("keyup",function(ev){
             fetch( 'server/chat_insert_post.php', options )
                 .then( response => console.log(response) )
         }
+        teclado.value = ""
     }
 })
 
 
 //lanzar un INTERVAL para hacer FETCH de nuevos mensajes de la BD
 // cada x milisegundos "chat_select_get_xml.php"
-consultarNuevosMensajes(0)
 function consultarNuevosMensajes(id) {
+    console.log("ultimoRecibido=" + id)
     fetch("server/chat_select_get_xml.php"+"?ultimo="+id)
     .then(respuesta => respuesta.text())
     .then(xmlCrudo => {
@@ -40,11 +43,22 @@ function consultarNuevosMensajes(id) {
             let nick = mensaje.children[1].textContent
             let texto = mensaje.children[2].textContent
             let instante = mensaje.children[3].textContent
+            //Actualizo "ultimoRecibido" para que la próxima consulta
+            // del interval pida solamente los mensajes nuevos
+            ultimoRecibido = id
             let nuevoMensaje = document.createElement("DIV")
             nuevoMensaje.classList.add("mensaje")
             nuevoMensaje.textContent = nick + ": " + texto
             chat.append(nuevoMensaje)
+            //Si hay muchos mensajes y no se ven todos,
+            // aparece la barra de scroll vertical.
+            // Forzamos a que vaya bajando el scroll automáticamente
+            chat.scrollTop = chat.scrollHeight
         });
     })
+    
 }
 
+let consultaMensajes = setInterval(function(){
+        consultarNuevosMensajes(ultimoRecibido)
+    },1000)
